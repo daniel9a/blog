c626a2dd7b9b7b144dc73709823af5f9481ac017
Daniel Andrade <andrade.daniel111@gmail.com>
2019-11-06 21:06:29 -0800

    aded a few pages and styles

---
+++ src/pages/teams.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const TeamsPage = () => (
+  <Layout>
+    <SEO title="Team" />
+    <h1>Teams Me</h1>
+  </Layout>
+)
+
+export default TeamsPage
