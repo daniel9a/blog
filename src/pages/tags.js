66c639a39e0cb3f1d79e8da4ba8720a1108bbdf3
daniel9a <andrade.daniel111@gmail.com>
2019-11-09 14:56:13 -0800

    refractored code

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,12 +1,10 @@
-import React from "react"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import React from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 const TagsPage = () => (
-  <Layout>
-    <SEO title="Tags" />
-    <h1>About Me</h1>
+  <Layout pageTitle="All topics">
+    <SEO title="Tags" keywords={[`gatsby`, `application`, `react`]} />
   </Layout>
 )
 
