1612898cb55a7d3c0b91056cf2e6508f90559687
daniel9a <andrade.daniel111@gmail.com>
2019-11-09 11:03:32 -0800

    added sidebar and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Footer = () => (
+  <div className="site-footer">
+    <h4 className="text-center">Code Blog</h4>
+    <p className="text-center">Follow us on social media</p>
+    <div className="footer-social-links">
+      <ul className="social-links-list">
+        <li>
+          <a
+            href="https://www.github.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="github"
+          >
+            <i className="fab fa-github fa-2x" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.google.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="google"
+          >
+            <i className="fab fa-google fa-2x" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="linkedin"
+          >
+            <i className="fab fa-linkedin fa-2x" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+)
+
+export default Footer
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,42 +1,39 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import Header from './header'
+import Footer from './Footer'
+import '../styles/index.scss'
 
-import Header from "./header"
-import "../styles/index.scss"
-
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
         }
       }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div className="container" id="content">
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
+    `}
+    render={data => (
+      <>
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+          integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+          crossOrigin="anonymous"
+        />
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div className="container" id="content">
+          {children}
+        </div>
+        <Footer />
+      </>
+    )}
+  />
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
