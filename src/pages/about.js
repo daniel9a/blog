60715486e9b45f0d67cc40d978242e65cf8ae786
daniel9a <andrade.daniel111@gmail.com>
2019-11-30 21:40:07 -0800

    edited about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import { Card, CardBody, CardSubtitle, CardHeader } from 'reactstrap'
+// import Img from 'gatsby-image'
+// import python from '../images/python.png'
 
 const AboutPage = () => (
   <Layout pageTitle="About us">
     <SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quibusdam
-      pariatur magnam nulla quis nobis rerum vitae in delectus modi
-      exercitationem amet quasi, distinctio illo blanditiis, adipisci, odio unde
-      minima numquam architecto! Odit sequi facere sit odio maiores possimus
-      ratione!
-    </p>
+    <Card>
+      {/* <img className="card-img-top" src={python} /> */}
+      <CardHeader><h1 className="text-center">About the Author</h1></CardHeader>
+      <CardBody>
+        <CardSubtitle><h3>About Me</h3></CardSubtitle>
+        <p>
+          I'm a Full-Time Software Engineer in San Mateo. I work with <b>C/C++, Python, & SQL</b> developing a healthcare delivery system.
+          I was first introduced to coding in high school during a Learn to Code after school event. I then went into college and went to majored
+          in Computer Science and Engineering at UC Davis.
+        </p>
+        <CardSubtitle><h3>Why Do I Blog?</h3></CardSubtitle>
+        <div className="list">
+          <p>I blog for a few reasons:</p>
+          <ul>
+            <li>Build my profile as a Developer</li>
+            <li>Hope to help developers</li>
+            <li>A place to keep notes and keep track of software development over time</li>
+          </ul>
+        </div>
+
+      </CardBody>
+    </Card>
   </Layout>
 )
 
--- src/util/authors.js
+++ src/util/authors.js
@@ -1,10 +1,18 @@
 const authors = [
   {
+    name: 'Daniel Andrade',
+    imageUrl: 'python.png',
+    bio:
+      'Software Engineer, always open to new opportunites',
+    github: 'https://www.github.com/daniel9a',
+    linkedin: 'https://www.linkedin.com/in/daniel-andrade9/',
+  },
+  {
     name: 'John Doe',
     imageUrl: 'python.png',
     bio:
       'John has been a front-end and UI designer fpr over 10 years, he is a brilliant artist',
-    instagram: 'https://www.github.com/',
+    github: 'https://www.github.com/',
     google: 'https://www.google.com/',
     linkedin: 'https://www.linkedin.com/',
   },
