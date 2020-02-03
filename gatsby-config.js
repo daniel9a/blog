3ec1d4e3a3a9613cfc4b6b822c77f8d0e913e15a
daniel9a <andrade.daniel111@gmail.com>
2020-02-03 11:26:11 -0800

    changed env in config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 require("dotenv").config({
-  // path: `.env.${process.env.NODE_ENV}`,
-  path: `.env.production`,
+  path: `.env.${process.env.NODE_ENV}`,
+  // path: `.env.production`,
 })
 
 const queries = require("./src/util/algolia")
